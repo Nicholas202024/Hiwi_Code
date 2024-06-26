bd09fe28d91080bdf393e09fd9e27aa0c768db19
Nicholas Illich <st172541@stud.uni-stuttgart.de>
2024-06-26 16:22:33 +0200

    Korrektur mit Primärstationen fertigstellen

--- Korrektur_Emscher.py
+++ Korrektur_Emscher.py
@@ -174,7 +174,7 @@ def find_all_stations_in_range(loc_prim, loc_sec, station, frame, radius, plot=T
         df_reference_stations = pd.DataFrame(array_reference_stations, columns=['lon', 'lat', 'distance'], index=list_station)
 
     if plot:
-        coordinates_all_stations_in_range(loc_prim, loc_sec, 'secondary', station, frame, 'circle', radius, df_reference_stations)
+        ke.coordinates_all_stations_in_range(loc_prim, loc_sec, 'secondary', station, frame, 'circle', radius, df_reference_stations)
        
     return df_reference_stations
 
@@ -306,14 +306,179 @@ def correction_emscher_complete(loc_prim, loc_sec, data, data_nonan, station_zah
 # 
 # 
 # 
-# Funktionen die zur Korrektur der Emscher Daten allein mit PWS benötigt werden 
+# Funktionen die zur Korrektur der Emscher Daten mit PWS oder Pluvio benötigt werden 
 # 
 # 
 # 
 # 
 ###
 
+def coordinates_all_stations_in_range_mit_primary(loc_prim, loc_sec, type_station, station, frame, geo, radius, ref_df, primary_found):
 
+    # name plot
+    name_plot = 'Plot Primär-/Sekundärnetzwerk: ' + type_station + 'station Nr. ' + str(station)
+
+    # beide Netzwerke plotten
+    plt.scatter(x=loc_sec['lon'], y=loc_sec['lat'], s=10, label='PWS')
+    plt.scatter(x=loc_prim['lon'], y=loc_prim['lat'], s=30, color='black', marker='x', label='Primärstationen')
+
+    # ausgewählte Station plotten
+    if type(station) == int:
+        if type_station == 'Primär':
+            plt.scatter(loc_prim['lon'].loc[station], loc_prim['lat'].loc[station], color='red', label='Ausgewählte Station')
+        elif type_station == 'Sekundär':
+            plt.scatter(loc_sec['lon'].loc[station], loc_sec['lat'].loc[station], color='red', label='Ausgewählte Station')
+            
+            # Referenzstationen plotten
+            if primary_found:
+                plt.scatter(loc_prim['lon'].loc[ref_df.index[0]], loc_prim['lat'].loc[ref_df.index[0]], color='lime', s=30, marker='x', label='Primärreferenzstatoin(-en)')
+                for station_ref in ref_df.index[1:]:
+                    plt.scatter(loc_prim['lon'].loc[station_ref], loc_prim['lat'].loc[station_ref], color='lime', s=30, marker='x')
+            else:
+                plt.scatter(loc_sec['lon'].loc[ref_df.index[0]], loc_sec['lat'].loc[ref_df.index[0]], color='lime', s=10, label='PWS-Referenzstation(-en)')
+                for station_ref in ref_df.index[1:]:
+                    plt.scatter(loc_sec['lon'].loc[station_ref], loc_sec['lat'].loc[station_ref], color='lime', s=10)
+
+    # plotte Kreis
+    if geo == 'circle':
+        kreis = Circle((loc_sec['lon'].loc[station], loc_sec['lat'].loc[station]), radius=radius, color='black', linewidth=0.5, fill=False)
+        plt.gca().add_patch(kreis)
+
+    plt.axis('equal')
+
+    # Anzeigebereich festlegen
+    if type(frame) == int:
+        try:
+            plt.xlim(loc_sec['lon'].loc[station] - frame, loc_sec['lon'].loc[station] + frame)
+        except:
+            if ((loc_sec['lon'].loc[station] - frame) < loc_sec['lon'].min()):
+                plt.xlim(loc_sec['lon'].min(), loc_sec['lon'].loc[station] + frame)
+            if ((loc_sec['lon'].loc[station] + frame) > loc_sec['lon'].max()):
+                plt.xlim(loc_sec['lon'].loc[station] - frame, loc_sec['lon'].max())
+        try:
+            plt.ylim(loc_sec['lat'].loc[station] - frame, loc_sec['lat'].loc[station] + frame)
+        except:
+            if ((loc_sec['lat'].loc[station] - frame) < loc_sec['lat'].min()):
+                plt.ylim(loc_sec['lat'].min(), loc_sec['lat'].loc[station] + frame)
+            if ((loc_sec['lat'].loc[station] + frame) > loc_sec['lat'].max()):
+                plt.ylim(loc_sec['lat'].loc[station] - frame, loc_sec['lat'].max())
+    else:
+        pass
+    
+    plt.legend()
+    plt.xlabel('longitude')
+    plt.ylabel('latitude')
+    plt.title(name_plot)
+
+    plt.show()
+    plt.close()
+    
+    return
+
+def find_all_stations_in_range_mit_primary(loc_prim, loc_sec, station, frame, radius, plot=True):
+
+    # finde stationen, die innerhalb eines bestimmten Bereich um die Station liegen
+
+    # koordinaten der station
+    lon_station = loc_sec['lon'].loc[station]
+    lat_station = loc_sec['lat'].loc[station]
+
+    list_reference_stations_lon = []
+    list_reference_stations_lat = []
+    list_station = []
+    list_distance = []
+
+    # check, ob Primärreferenz innerhalb Radius
+    primary_found = False
+
+    for i in loc_prim.index:
+        lon = loc_prim['lon'].loc[i]
+        lat = loc_prim['lat'].loc[i]
+        if (np.sqrt((lon - lon_station)**2 + (lat - lat_station)**2) <= radius):
+
+            distance = round(np.sqrt((lon - lon_station)**2 + (lat - lat_station)**2), 2)
+                    
+            list_distance.append(distance)
+            list_station.append(i)
+            list_reference_stations_lon.append(round(lon, 2))
+            list_reference_stations_lat.append(round(lat, 2))
+
+    if list_station:
+        primary_found = True
+
+    # finde die nächsten PWS, wenn keine Primärreferenz gefunden wurde
+    if primary_found:
+        pass
+    else:
+        for i in loc_sec.index:
+            lon = loc_sec['lon'].loc[i]
+            lat = loc_sec['lat'].loc[i]
+            if (np.sqrt((lon - lon_station)**2 + (lat - lat_station)**2) <= radius):
+                if lon == lon_station and lat == lat_station:
+                    pass
+                else:
+                    distance = round(np.sqrt((lon - lon_station)**2 + (lat - lat_station)**2), 2)
+                    
+                    list_distance.append(distance)
+                    list_station.append(i)
+                    list_reference_stations_lon.append(round(lon, 2))
+                    list_reference_stations_lat.append(round(lat, 2))
+        
+    array_reference_stations = np.array([list_reference_stations_lon, list_reference_stations_lat, list_distance]).T
+    df_reference_stations = pd.DataFrame(array_reference_stations, columns=['lon', 'lat', 'distance'], index=list_station)
+
+    if plot:
+        ke.coordinates_all_stations_in_range_mit_primary(loc_prim, loc_sec, 'Sekundär', station, frame, 'circle', radius, df_reference_stations, primary_found)
+       
+    return df_reference_stations, primary_found
+
+def berechnung_referenzniederschlag_emscher_mit_primary(df_reference_stations, data_sec, data_prim_nonan, data_sec_nonan, station_zahl, starts_nan_seq_mit_peak, peaks_mit_nan_seq, primary_found):
+    
+    # df zum speichern der Referenzniederschläge erstellen
+    df_reference_values = data_sec[[station_zahl]].copy()
+
+    if primary_found:
+        ref_station_values = data_prim_nonan
+    else:
+        ref_station_values = data_sec_nonan
+
+    # abrufen der Informationen über NaN-Sequenz und Peaks
+    # starts_nan_seq_mit_peak, peaks_mit_nan_seq = get_data_nan_seq_before_peak_new(data, station_zahl, 0.99)
+
+    # berechnung Referenzniederschlag
+    # für jede NaN-Sequenz und Peak
+    for c, p in zip(range(0, len(starts_nan_seq_mit_peak)), range(0, len(peaks_mit_nan_seq))):
+        # für jeden Index in der NaN-Sequenz
+        for index in ref_station_values.loc[starts_nan_seq_mit_peak[c] : peaks_mit_nan_seq[p]].index:
+            h_ref = 0
+            # für jede Referenzstation
+            for ref_station in df_reference_stations.index:
+                h_ref += ref_station_values[ref_station].loc[index] * df_reference_stations['weight'].loc[ref_station]
+
+            # speichern des Referenzniederschlags für den Index
+            df_reference_values.loc[index] = h_ref
+
+        # alle Werte außerhalb der NaN-Sequenz und Peaks auf NaN setzen
+        if c == 0 and p == 0:
+            df_reference_values.loc[(df_reference_values.index[0]) : (starts_nan_seq_mit_peak[c] - datetime.timedelta(minutes=5))] = np.nan
+        if (c > 0 and p > 0) and ((c < len(starts_nan_seq_mit_peak) - 1) and (p < len(peaks_mit_nan_seq) - 1)):
+            df_reference_values.loc[(peaks_mit_nan_seq[p - 1] + datetime.timedelta(minutes=5)) : (starts_nan_seq_mit_peak[c] - datetime.timedelta(minutes=5))] = np.nan
+        if (c == len(starts_nan_seq_mit_peak) - 1) and (p == len(peaks_mit_nan_seq) - 1):
+            df_reference_values.loc[(peaks_mit_nan_seq[p] + datetime.timedelta(minutes=5)) : (df_reference_values.index[-1])] = np.nan
+    
+    return df_reference_values
+
+def correction_emscher_complete_mit_primary(loc_prim, loc_sec, data_sec, data_prim_nonan, data_sec_nonan, station_zahl, frame, radius, quantile, plot=False, correct_peak=True, correct_1_2=True, correct_0_pres_ref=True):
+    df_reference_stations, primary_found = ke.find_all_stations_in_range_mit_primary(loc_prim, loc_sec, station_zahl, frame, radius, plot)
+    if df_reference_stations.empty:
+        print('No reference stations found within radius')
+        return
+    df_reference_stations = ke.berechnung_gewichte_emscher(df_reference_stations)
+    starts_nan_seq_mit_peak, peaks_mit_nan_seq = ke.get_data_nan_seq_before_peak_new(data_sec, station_zahl, quantile)
+    df_reference_values = ke.berechnung_referenzniederschlag_emscher_mit_primary(df_reference_stations, data_sec, data_prim_nonan, data_sec_nonan, station_zahl, starts_nan_seq_mit_peak, peaks_mit_nan_seq, primary_found)
+    data_corrected = ke.correct_data_new_emscher(data_sec, df_reference_values, station_zahl, starts_nan_seq_mit_peak, peaks_mit_nan_seq, correct_peak, correct_1_2, correct_0_pres_ref)
+
+    return data_corrected
 
 ###
 # 
@@ -327,3 +492,54 @@ def correction_emscher_complete(loc_prim, loc_sec, data, data_nonan, station_zah
 # 
 ###
 
+def get_statistics_emscher(data_uncorrected, data_corrected):
+
+    list_nans_gesamt = []
+    list_nan_sequences = []
+    list_nan_sequences_1_2 = []
+    list_nan_sequences_1_2_corr = []
+    list_peaks_u = []
+    list_peaks = []
+    list_nans_gesamt_corr = []
+    list_nan_sequences_corr = []
+    list_verhaeltnis_nans = []
+    list_verhaeltnis_nan_sequences = []
+
+    for station in data_corrected.columns:
+        sum_nan_u = data_uncorrected[station].isna().sum()
+        list_u = ke.list_nan_sequences_schnell(data_uncorrected, station, '5min')[2]
+        x_u, y_u = np.unique(list_u, return_counts=True)
+        peaks_u = ke.get_data_nan_seq_before_peak_new(data_uncorrected, station, 0.99)[1]
+
+        # print(station, 'unkorrigiert')
+        # print('   nans gesamt: ', sum_nan_u)
+        # print('   nan sequenzen: ', y_u.sum())
+        # print('   davon 1er und 2er nan sequenzen: ', y_u[0:2].sum())
+        # print('   peaks: ', len(peaks_u))
+
+        list_nans_gesamt.append(sum_nan_u)
+        list_nan_sequences.append(y_u.sum())
+        list_nan_sequences_1_2.append(y_u[0:2].sum())
+        list_peaks_u.append(len(peaks_u))
+
+        sum_nan = data_corrected[station].isna().sum()
+        list = ke.list_nan_sequences_schnell(data_corrected, station, '5min')[2]
+        x, y = np.unique(list, return_counts=True)
+        peaks = ke.get_data_nan_seq_before_peak_new(data_corrected, station, 0.99)[1]
+
+        # print(station, 'korrigiert')
+        # print('   nans gesamt: ', sum_nan)
+        # print('   nan sequenzen: ', y.sum())
+        # print('Verhältnis korrigiert zu unkorrigiert: ', round(((sum_nan_u - sum_nan)/sum_nan_u)*100, 2), '% werden korrigiert')
+
+        list_nans_gesamt_corr.append(sum_nan)
+        list_nan_sequences_corr.append(y.sum())
+        list_verhaeltnis_nans.append(round(((sum_nan_u - sum_nan)/sum_nan_u)*100))
+        list_verhaeltnis_nan_sequences.append(round(((y_u.sum() - y.sum())/y_u.sum())*100))
+        list_peaks.append(len(peaks))
+        list_nan_sequences_1_2_corr.append(y[0:2].sum())
+
+    df_emscher_statistik = pd.DataFrame(index=['NaNs gesamt', 'NaN-Sequenzen', '1er und 2er NaN-Sequenzen', 'Peaks', '--------------------------------------------------', 'NaNs nach Korrektur', 'NaN-Sequenzen nach Korrektur', '1er und 2er NaN-Sequenzen nach Korrektur', 'Peaks nach Korrektur', '--------------------------------------------------', '% NaNs korrigiert', '% NaN-Sequenzen korrigiert'], data=[list_nans_gesamt, list_nan_sequences, list_nan_sequences_1_2, list_peaks_u, [], list_nans_gesamt_corr, list_nan_sequences_corr, list_nan_sequences_1_2_corr, list_peaks, [], list_verhaeltnis_nans, list_verhaeltnis_nan_sequences], columns=data_corrected.columns)
+    df_emscher_statistik = df_emscher_statistik.fillna('')
+
+    return df_emscher_statistik
